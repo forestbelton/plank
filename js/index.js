089e8863e23bed0324fabafc4474d8793d1bca07
Forest Belton <forest@homolo.gy>
2017-07-18 16:57:28 -0700

    Move main CSS file into lib/

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './main.css';
+import './lib/main.css';
 
 import HttpApi from './lib/api/HttpApi';
 import BoardPage from './lib/components/BoardPage';
